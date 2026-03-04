9b341781f63d7ea6871780641f34d72ca7ea1d37
wqar ul hassan <waqarjhang005@gmail.com>
2026-03-04 06:00:10 +0500

    sidebar responsive update

--- src/components/common/HomeSection.jsx
+++ src/components/common/HomeSection.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Home from '../../pages/home/Home'
 import Footer from './Footer'
-import Sidebar from './Sidebar'
-import Loader from './Loader'
+
 
 export default function HomeSection() {
   return (
